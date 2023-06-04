af6b49ffc0603fa8ef4db75f8aad2cdb3a14fb0a
Dmitrii <121618870+1132988@users.noreply.github.com>
2023-06-04 12:53:29 +0300

    edits

---
+++ task5.py
@@ -0,0 +1,4 @@
+import heapq
+#Найдите три ключа с самыми высокими значениями в словаре my_dict = {'a':500, 'b':5874, 'c': 560,'d':400, 'e':5874, 'f': 20}.import heapq
+my_dict = {'a': 500, 'b': 5874, 'c': 560, 'd': 400, 'e': 5874, 'f': 20}
+print(heapq.nlargest(3, my_dict, key=lambda k: my_dict[k]))
